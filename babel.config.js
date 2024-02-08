833b4c3a62ce5c64ce9d5f1a0fa26028992d4579
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-07 23:23:27 -0300

    chore: path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+// https://www.npmjs.com/package/babel-plugin-module-resolver
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@theme": "./src/theme",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
