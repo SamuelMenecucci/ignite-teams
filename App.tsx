979fa929744d645d1367848deb4a45b896830472
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-13 15:49:43 -0300

    chore: setting for using external font-family

--- App.tsx
+++ App.tsx
@@ -1,9 +1,20 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+
   return (
     <ThemeProvider theme={theme}>
       <View style={styles.container}>
