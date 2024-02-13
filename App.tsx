4eae9dab0449b8a476add542bb957212d51162ef
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-13 16:32:37 -0300

    feat: using groups and loading component in app.tsx

--- App.tsx
+++ App.tsx
@@ -7,29 +7,15 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
 import theme from "./src/theme";
+import { Loading } from "@components/Loading";
+import { Groups } from "@screens/Groups";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
-  if (!fontsLoaded) {
-    return <ActivityIndicator />;
-  }
-
   return (
     <ThemeProvider theme={theme}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
