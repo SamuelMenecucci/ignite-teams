f608b1068bf8128e7ac712abaae331623492ff66
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-15 20:35:09 -0300

    style: placeholder style

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,11 @@
 import { Container } from "./styles";
 import { TextInputProps } from "react-native";
 
+//o useTheme permite que eu acesse o meu tema por outros lugares da minha aplicação. por exemplo, toda atribuição de algum valor do tema como fonte, cores e etc foram feitas até agora pelo meu arquivo styles.ts. com o useTheme eu consigo fazer o uso do meu tema fora de um componente do styled-component, dentro do styles.ts
+import { useTheme } from "styled-components/native";
+
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const { COLORS } = useTheme();
+
+  return <Container placeholderTextColor={COLORS.GRAY_300} {...rest} />;
 }
