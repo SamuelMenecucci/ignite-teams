f4f66623fea4aefba84272544b47400d79206ecb
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-02-29 07:43:44 -0300

    feat: input and buttonicon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
 export function Players() {
   return (
@@ -11,6 +13,15 @@ export function Players() {
         title="Nome da turma"
         subtitle="adicione a galera e separe os times"
       />
+
+      <Form>
+        <Input
+          placeholder="Nome da pessoa"
+          //como o input é referente ao nome de uma pessoa, desabilito o autoCorrect para que não tenhamos problema com o corretor, no caso de algum apelido ou algo do tipo.
+          autoCorrect={false}
+        />
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   );
 }
