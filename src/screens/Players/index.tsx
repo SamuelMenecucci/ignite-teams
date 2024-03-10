57bc7d651ac389961f4987418a047537271277ae
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-03-10 18:33:08 -0300

    feat: passing and obtaining parameters along the route

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,3 +1,4 @@
+import { useRoute } from "@react-navigation/native";
 import { FlatList } from "react-native";
 import { Header } from "@components/Header";
 import { Container, Form, HeaderList, NumberOfPlayers } from "./styles";
@@ -10,18 +11,23 @@ import { PlayerCard } from "@components/PlayerCard";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
 
+type RouteParams = {
+  group: string;
+};
+
 export function Players() {
   const [team, setTeam] = useState("Time A");
   const [players, setPlayers] = useState(["Samuel"]);
+
+  //para acessar parametros passados pela rota eu utilizo o hook useRoute do @react-navigation/native
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
 
   return (
     <Container>
       <Header showBackButton />
 
-      <Highlight
-        title="Nome da turma"
-        subtitle="adicione a galera e separe os times"
-      />
+      <Highlight title={group} subtitle="adicione a galera e separe os times" />
 
       <Form>
         <Input
