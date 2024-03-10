5551b8c53c4709c764a820d135a0d8b211ab565c
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-03-10 20:29:10 -0300

    feat: creating and fetching values with async storage

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { Container } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -6,8 +6,8 @@ import { GroupCard } from "@components/GroupCard";
 import { FlatList } from "react-native";
 import { ListEmpty } from "@components/ListEmpty";
 import { Button } from "@components/Button";
-import { useNavigation } from "@react-navigation/native";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation, useFocusEffect } from "@react-navigation/native";
+import { groupsGetAll } from "@storage/group/groupsGetAll";
 
 // type RootParamList = {
 //   //dentro do RootParamList eu coloco o nome da rota e se ela tem algum parametro. caso ela não tenha parametro eu passo como undefined, caso tenha, passo um objeto com os parametros que irei receber.
@@ -32,6 +32,25 @@ export function Groups() {
     //as opções de rota aparecem por conta da tipagem e em @types/navigation.d.ts
     navigation.navigate("new");
   };
+
+  async function fetchGroups() {
+    try {
+      const fetched = await groupsGetAll();
+
+      setGroups(fetched);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //para fazer o fetch dos grupos que estão no device eu por padrão utilizaria o useEffect, que dispara uma função toda vez que o componente renderiza ou toda vez que algum dado que está dentro do array de dependencia muda. no react-native se eu utilizar o useeffect, ao voltar para a tela inicial ele não será disparado novamente, somente a primeira vez que o usuário abrir o app e entrar na tela em questão. isso ocorre pois a tela, após a primeira renderização ela não é renderizada novamente, mesmo que o usuário tenha ido para outra tela e voltado. para resolver isso, eu utilizo o useFocusEffect do @react-navigation/native". com ele eu tenho o disparo da função que é passada como primeiro parametro toda vez que essa tela estiver em foco novamente. um ponto importante é que eu preciso passar essa função que ele recebe como parametro dentro do useCallback. pois dessa forma irá evitar chamadas desnecessárias da função que ele está guardando.
+  //DOC useFocusEffect https://reactnavigation.org/docs/use-focus-effect/
+  //DOC useCallback https://react.dev/reference/react/useCallback
+  useFocusEffect(
+    useCallback(() => {
+      fetchGroups();
+    }, [])
+  );
 
   return (
     <Container>
