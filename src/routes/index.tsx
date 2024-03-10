3fb41795456beb2718444580de01ead9e743a1e0
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-03-10 18:39:17 -0300

    feat: minimizing glitch when changing route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,19 @@
 //iremos criar esse arquivo como um componente para utilizarmos dentro do app, pois é por onde a aplicação passa quando é executada
 import { NavigationContainer } from "@react-navigation/native";
 import { AppRoutes } from "./app.routes";
+import { View } from "react-native";
+import { useTheme } from "styled-components/native";
 
 export function Routes() {
+  const { COLORS } = useTheme();
+
+  //ao trocarmos de rota conseguimos perceber um glitch na aplicação. ao troca a rota é nos mostrado um fundo branco enquanto a troca é feita. para minimizarmos esse glitch ao navegar pela aplicação iremos colcoar uma view por volta do navigation container e iremos colocar o background-color padrão da nossa aplicação, dessa forma minimizamos esse glitch.
+
   return (
-    <NavigationContainer>
-      <AppRoutes />
-    </NavigationContainer>
+    <View style={{ flex: 1, backgroundColor: COLORS.GRAY_600 }}>
+      <NavigationContainer>
+        <AppRoutes />
+      </NavigationContainer>
+    </View>
   );
 }
