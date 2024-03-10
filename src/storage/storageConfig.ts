8457a4148352d1c73b2f55b267081a377cf0042d
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-03-10 20:28:31 -0300

    feat: functions to create and retrieve values in async storage

---
+++ src/storage/storageConfig.ts
@@ -0,0 +1,5 @@
+//crio um arquivo especifico com as chaves que utilizarei no async storage pois independente da função que eu utilizar (salvar, remover ou recuperar) a chave será a mesma sempre, caso seja do mesmo contexto, dessa forma não preciso ficar escrevendo coisa repetida.
+
+const GROUP_COLLECTION = "@ignite-teams:groups";
+
+export { GROUP_COLLECTION };
