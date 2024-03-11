33645792653fd7e6890db16f17d3c584cc5acbfd
Samuel Menecucci Moura <samuelmenecucci@gmail.com>
2024-03-10 21:24:20 -0300

    feat: players function async storage

--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,5 +1,6 @@
 //crio um arquivo especifico com as chaves que utilizarei no async storage pois independente da função que eu utilizar (salvar, remover ou recuperar) a chave será a mesma sempre, caso seja do mesmo contexto, dessa forma não preciso ficar escrevendo coisa repetida.
 
 const GROUP_COLLECTION = "@ignite-teams:groups";
+const PLAYER_COLLECTION = "@ignite-teams:players";
 
-export { GROUP_COLLECTION };
+export { GROUP_COLLECTION, PLAYER_COLLECTION };
